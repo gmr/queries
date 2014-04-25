a3b4b1c052941b9386664b4c7092b6417464ba9c
Gavin M. Roy <gavinr@aweber.com>
2014-04-25 13:01:15 -0400

    Updated documentation and class functions

--- queries/session.py
+++ queries/session.py
@@ -1,29 +1,61 @@
-"""PostgreSQL Class module
+"""The Session class allows for a unified (and simplified) view of
+interfacing with a PostgreSQL database server.
 
+Connection details are passed in as a PostgreSQL URI and connections are pooled
+by default, allowing for reuse of connections across modules in the Python
+runtime without having to pass around the object handle.
+
+While you can still access the raw `psycopg2` connection and cursor objects to
+provide ultimate flexibility in how you use the queries.Session object, there
+are convenience methods designed to simplify the interaction with PostgreSQL.
+For `psycopg2` functionality outside of what is exposed in Session, simply
+use the Session.connection or Session.cursor properties to gain access to
+either object just as you would in a program using psycopg2 directly.
 
 
 """
+import contextlib
 import logging
 
 import psycopg2
 from psycopg2 import extensions
 from psycopg2 import extras
 
-from queries import pool, PYPY
+from queries import pool
+from queries import utils
 
 LOGGER = logging.getLogger(__name__)
 
+from queries import DEFAULT_URI
+from queries import PYPY
+
+DEFAULT_ENCODING = 'UTF8'
+
+
 
 class Session(object):
-    """Core queries
+    """The Session class allows for a unified (and simplified) view of
+    interfacing with a PostgreSQL database server. The Session object can
+    act as a context manager, providing automated cleanup and simple, pythoic
+    way of interacting with the object:
 
-    Uses a module level cache of connections to reduce overhead.
+    .. code:: python
+
+        import queries
+
+        with queries.Session('pgsql://postgres@localhost/postgres') as session:
+            for row in session.Query('SELECT * FROM table'):
+                print row
+
+    :param str uri: PostgreSQL connection URI
+    :param psycopg2.cursor: The cursor type to use
+    :param bool use_pool: Use the connection pool
 
     """
     _from_pool = False
+    _tpc_id = None
 
     # Connection status constants
-    BEGIN = extensions.STATUS_BEGIN
     INTRANS = extensions.STATUS_IN_TRANSACTION
     PREPARED = extensions.STATUS_PREPARED
     READY = extensions.STATUS_READY
@@ -35,7 +67,7 @@ class Session(object):
     TX_INTRANS = extensions.TRANSACTION_STATUS_INTRANS
     TX_UNKNOWN = extensions.TRANSACTION_STATUS_UNKNOWN
 
-    def __init__(self, uri,
+    def __init__(self, uri=DEFAULT_URI,
                  cursor_factory=extras.RealDictCursor,
                  use_pool=True):
         """Connect to a PostgreSQL server using the module wide connection and
@@ -52,17 +84,13 @@ class Session(object):
         self._cursor = self._get_cursor(cursor_factory)
         self._autocommit()
 
-        # Don't re-register unicode or uuid
-        if not use_pool or not self._from_pool:
-            self._register_unicode()
-            self._register_uuid()
-
     def __del__(self):
         """When deleting the context, ensure the instance is removed from
         caches, etc.
 
         """
-        self._cleanup()
+        #self._cleanup()
+        pass
 
     def __enter__(self):
         """For use as a context manager, return a handle to this object
@@ -78,11 +106,33 @@ class Session(object):
         caches, etc.
 
         """
-        self._cleanup()
+        #self._cleanup()
+        pass
 
     @property
     def backend_pid(self):
+        """Return the backend process ID of the PostgreSQL server that this
+        session is connected to.
+
+        :rtype: int
+
+        """
         return self._conn.get_backend_pid()
+
+    def close(self):
+        """Explicitly close the connection and remove it from the connection
+        pool if pooling is enabled.
+
+        :raises: AssertionError
+
+        """
+        if not self._conn:
+            raise AssertionError('Connection not open')
+        self._conn.close()
+        if self._use_pool:
+            pool.remove_connection(self._uri)
+        self._conn = None
+        self._cursor = None
 
     @property
     def connection(self):
@@ -102,36 +152,134 @@ class Session(object):
         """
         return self._cursor
 
-    def cancel(self):
-        self._conn.cancel()
-
-    def commit(self):
-        self._conn.commit()
-
-    def listen(self, channel):
-        pass
-
     @property
     def encoding(self):
+        """The current client encoding value.
+
+        :rtype: str
+
+        """
         return self._conn.encoding
-
-    def set_encoding(self, value='UTF-8'):
-        self._conn.set_client_encoding(value)
-
-    def rollback(self):
-        self._conn.rollback()
-
-    def create_transaction(self):
-        self._conn.autocommit = False
-        self._cursor.execute('')
 
     @property
     def notices(self):
+        """A list of up to the last 50 server notices sent to the client.
+
+        :rtype: list
+
+        """
         return self._conn.notices
+
+    def set_encoding(self, value=DEFAULT_ENCODING):
+        """Set the client encoding for the session if the value specified
+        is different than the current client encoding.
+
+        :param str value: The encoding value to use
+
+        """
+        if self._conn.encoding != value:
+            self._conn.set_client_encoding(value)
 
     @property
     def status(self):
+        """Return the current connection status as an integer value.
+
+        The status should match one of the following constants:
+
+        - queries.Session.INTRANS: Connection established, in transaction
+        - queries.Session.PREPARED: Prepared for second phase of transaction
+        - queries.Session.READY: Connected, no active transaction
+
+        :rtype: int
+
+        """
+        if self._conn.status == psycopg2.extensions.STATUS_BEGIN:
+            return self.READY
         return self._conn.status
+
+    # Querying, executing, copying, etc
+
+    def callproc(self, name, parameters=None):
+        """Call a stored procedure on the server and return an iterator of the
+        result set for easy access to the data.
+
+        .. code:: python
+
+            for row in session.callproc('now'):
+                print row
+
+        :param str name: The procedure name
+        :param list parameters: The list of parameters to pass in
+        :return: iterator
+
+        """
+        self._cursor.callproc(name, parameters)
+        for record in self._cursor:
+            yield record
+
+    def callproc_all(self, name, parameters=None):
+        """Call a stored procedure on the server returning all of the rows
+        returned by the server.
+
+        :rtype: list
+
+        """
+        self._cursor.callproc(name, parameters)
+        return self._cursor.fetchall()
+
+    def query(self, sql, parameters=None):
+        """A generator to issue a query on the server, mogrifying the
+        parameters against the sql statement and returning the results as an
+        iterator.
+
+        .. code:: python
+
+            for row in session.query('SELECT * FROM foo WHERE bar=%(bar)s',
+                                     {'bar': 'baz'}):
+              print row
+
+        :param str sql: The SQL statement
+        :param dict parameters: A dictionary of query parameters
+        :rtype: iterator
+
+        """
+        self._cursor.execute(sql, parameters)
+        for record in self._cursor:
+            yield record
+
+    def query_all(self, sql, parameters=None):
+        """Issue a query on the server mogrifying the parameters against the
+        sql statement and returning the entire result set as a list.
+
+        :param str sql: The SQL statement
+        :param dict parameters: A dictionary of query parameters
+        :rtype: list
+
+        """
+        self._cursor.execute(sql, parameters or {})
+        return self._cursor.fetchall()
+
+    # Listen Notify
+
+    def listen(self, channel, callback=None):
+        pass
+
+    def notifications(self):
+        pass
+
+    # TPC Transaction Functionality
+
+    def tx_begin(self):
+        """Begin a new transaction"""
+        # Ensure that auto-commit is off
+        if self._conn.autocommit:
+            self._conn.autocommit = False
+
+    def tx_commit(self):
+        self._conn.commit()
+
+    def tx_rollback(self):
+        self._conn.rollback()
 
     @property
     def tx_status(self):
@@ -150,20 +298,7 @@ class Session(object):
         """
         return self._conn.get_transaction_status()
 
-    def close(self):
-        """Explicitly close the connection and remove it from the connection
-        cache.
-
-        :raises: AssertionError
-
-        """
-        if not self._conn:
-            raise AssertionError('Connection not open')
-        self._conn.close()
-        if self._use_pool:
-            pool.remove_connection(self._uri)
-        self._conn = None
-        self._cursor = None
+    # Internal methods
 
     def _autocommit(self):
         """Set the isolation level automatically to commit after every query"""
@@ -193,7 +328,8 @@ class Session(object):
                 return connection
 
         # Create a new PostgreSQL connection
-        connection = psycopg2.connect(self._uri)
+        LOGGER.debug('Connection KWARGS: %r', utils.uri_to_kwargs(self._uri))
+        connection = psycopg2.connect(**utils.uri_to_kwargs(self._uri))
 
         # Add it to the pool, if pooling is enabled
         if self._use_pool:
@@ -204,6 +340,11 @@ class Session(object):
         # Connection._setup without setting the state as const.STATUS_OK
         if PYPY:
             connection.reset()
+
+        # Register the custom data types
+        self._register_json(connection)
+        self._register_unicode(connection)
+        self._register_uuid(connection)
 
         return connection
 
@@ -216,17 +357,32 @@ class Session(object):
         """
         return self._conn.cursor(cursor_factory=cursor_factory)
 
-    def _register_unicode(self):
+    @staticmethod
+    def _register_json(connection):
+        """Register the JSON extension from the psycopg2.extras module
+
+        :param psycopg2.connection connection: The connection to register on
+
+        """
+        psycopg2.extras.register_json(conn_or_curs=connection)
+
+    @staticmethod
+    def _register_unicode(connection):
         """Register the cursor to be able to receive Unicode string.
 
-        :param psycopg2.cursor: The cursor to add unicode support to
+        :param psycopg2.connection connection: The connection to register on
 
         """
         psycopg2.extensions.register_type(psycopg2.extensions.UNICODE,
-                                          self._cursor)
+                                          connection)
         psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY,
-                                          self._cursor)
+                                          connection)
 
-    def _register_uuid(self):
-        """Register the UUID extension from psycopg2"""
-        psycopg2.extras.register_uuid(self._conn)
+    @staticmethod
+    def _register_uuid(connection):
+        """Register the UUID extension from the psycopg2.extra module
+
+        :param psycopg2.connection connection: The connection to register on
+
+        """
+        psycopg2.extras.register_uuid(conn_or_curs=connection)
