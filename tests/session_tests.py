2dc0ad5307f20adae8885408063f733203760f86
Gavin M. Roy <gavinr@aweber.com>
2014-07-30 14:53:41 -0400

    Rework and complete session tests

--- tests/session_tests.py
+++ tests/session_tests.py
@@ -19,50 +19,57 @@ from queries import session
 from queries import PYPY
 
 
-class SessionTests(unittest.TestCase):
+class SessionTestCase(unittest.TestCase):
 
     URI = 'pgsql://foo:bar@localhost:5432/foo'
 
-
-    def setUp(self):
+    @mock.patch('psycopg2.connect')
+    @mock.patch('psycopg2.extensions.register_type')
+    @mock.patch('psycopg2.extras.register_uuid')
+    @mock.patch('queries.utils.uri_to_kwargs')
+    def setUp(self, uri_to_kwargs, register_uuid, register_type, connect):
         self.conn = mock.Mock()
-        self._connect = mock.Mock(return_value=self.conn)
-        self.cursor = mock.Mock()
-        self._get_cursor = mock.Mock(return_value=self.cursor)
-        self._autocommit = mock.Mock()
-        with mock.patch.multiple('queries.session.Session',
-                                 _connect=self._connect,
-                                 _get_cursor=self._get_cursor,
-                                 _autocommit=self._autocommit):
-            pool.PoolManager.create = self.pm_create = mock.Mock()
-            pool.PoolManager.is_full = self.pm_is_full = mock.PropertyMock()
-            pool.PoolManager.remove_connection = self.pm_rem_conn = mock.Mock()
-            self.obj = session.Session(self.URI)
+        self.conn.autocommit = False
+        self.conn.closed = False
+        self.conn.cursor = mock.Mock()
+        self.conn.isexecuting = mock.Mock(return_value=False)
+        self.conn.reset = mock.Mock()
+        self.conn.status = psycopg2.extensions.STATUS_BEGIN
+        self.psycopg2_connect = connect
+        self.psycopg2_connect.return_value = self.conn
+        self.psycopg2_register_type = register_type
+        self.psycopg2_register_uuid = register_uuid
 
-    def test_init_gets_poolmanager_instance(self):
-        instance = pool.PoolManager.instance()
-        self.assertEqual(self.obj._pool_manager, instance)
+        self.uri_to_kwargs = uri_to_kwargs
+        self.uri_to_kwargs.return_value = {'host': 'localhost',
+                                           'port': 5432,
+                                           'user': 'foo',
+                                           'password': 'bar',
+                                           'dbname': 'foo'}
+
+        self.obj = session.Session(self.URI)
 
     def test_init_sets_uri(self):
         self.assertEqual(self.obj._uri, self.URI)
 
-    def test_init_invokes_poolmanager_create(self):
-        self.obj._pool_manager.create.assert_called_once_with(
-            self.obj.pid,
-            pool.DEFAULT_IDLE_TTL,
-            pool.DEFAULT_MAX_SIZE)
+    def test_init_creates_new_pool(self):
+        self.assertIn(self.obj.pid, self.obj._pool_manager)
 
-    def test_init_invokes_connection(self):
-        self._connect.assert_called_once_with()
+    def test_init_creates_connection(self):
+        conns = \
+            [value.handle for key, value in
+             self.obj._pool_manager._pools[self.obj.pid].connections.items()]
+        self.assertIn(self.conn, conns)
 
     def test_init_sets_cursorfactory(self):
         self.assertEqual(self.obj._cursor_factory, extras.RealDictCursor)
 
     def test_init_gets_cursor(self):
-        self._get_cursor.assert_called_once_with(self.conn)
+        self.conn.cursor.assert_called_once_with(
+            name=None, cursor_factory=extras.RealDictCursor)
 
     def test_init_sets_autocommit(self):
-        self._autocommit.assert_called_once_with()
+        self.assertTrue(self.conn.autocommit)
 
     def test_backend_pid_invokes_conn_backend_pid(self):
         self.conn.get_backend_pid = get_backend_pid = mock.Mock()
@@ -70,13 +77,13 @@ class SessionTests(unittest.TestCase):
         get_backend_pid.assert_called_once_with()
 
     def test_callproc_invokes_cursor_callproc(self):
-        self.cursor.callproc = mock.Mock()
+        self.obj._cursor.callproc = mock.Mock()
         args = ('foo', ['bar', 'baz'])
         self.obj.callproc(*args)
-        self.cursor.callproc.assert_called_once_with(*args)
+        self.obj._cursor.callproc.assert_called_once_with(*args)
 
     def test_callproc_returns_results(self):
-        self.cursor.callproc = mock.Mock()
+        self.obj._cursor.callproc = mock.Mock()
         args = ('foo', ['bar', 'baz'])
         self.assertIsInstance(self.obj.callproc(*args), results.Results)
 
@@ -86,7 +93,8 @@ class SessionTests(unittest.TestCase):
 
     def test_close_removes_connection(self):
         self.obj.close()
-        self.pm_rem_conn.assert_called_once_with(self.obj.pid, self.conn)
+        self.assertNotIn(self.conn,
+                         self.obj._pool_manager._pools[self.obj.pid])
 
     def test_close_unassigns_connection(self):
         self.obj.close()
@@ -100,7 +108,7 @@ class SessionTests(unittest.TestCase):
         self.assertEqual(self.obj.connection, self.conn)
 
     def test_cursor_property_returns_correct_value(self):
-        self.assertEqual(self.obj.cursor, self.cursor)
+        self.assertEqual(self.obj.cursor, self.obj._cursor)
 
     def test_encoding_property_value(self):
         self.conn.encoding = 'UTF-8'
@@ -115,10 +123,10 @@ class SessionTests(unittest.TestCase):
         self.assertEqual(self.obj.pid, expectation)
 
     def test_query_invokes_cursor_execute(self):
-        self.cursor.execute = mock.Mock()
+        self.obj._cursor.callproc = mock.Mock()
         args = ('SELECT * FROM foo', ['bar', 'baz'])
         self.obj.query(*args)
-        self.cursor.execute.assert_called_once_with(*args)
+        self.obj._cursor.execute.assert_called_once_with(*args)
 
     def test_set_encoding_sets_encoding_if_different(self):
         self.conn.encoding = 'LATIN-1'
@@ -161,23 +169,24 @@ class SessionTests(unittest.TestCase):
         self.assertTrue(self.conn.autocommit)
 
     def test_cleanup_closes_cursor(self):
-        self.cursor.close = closeit = mock.Mock()
+        self.obj._cursor.close = closeit = mock.Mock()
         self.conn = None
         self.obj._cleanup()
         closeit.assert_called_once_with()
 
     def test_cleanup_sets_cursor_to_none(self):
-        self.cursor.close = mock.Mock()
+        self.obj._cursor.close = mock.Mock()
         self.conn = None
         self.obj._cleanup()
         self.assertIsNone(self.obj._cursor)
 
     def test_cleanup_frees_connection(self):
-        pool.PoolManager.free = free = mock.Mock()
-        self.obj._cleanup()
-        free.assert_called_once_with(self.obj.pid, self.conn)
+        with mock.patch.object(self.obj._pool_manager, 'free') as free:
+            conn = self.obj._conn
+            self.obj._cleanup()
+            free.assert_called_once_with(self.obj.pid, conn)
 
-    def test_cleanup_sets_connecto_to_none(self):
+    def test_cleanup_sets_connect_to_none(self):
         self.obj._cleanup()
         self.assertIsNone(self.obj._conn)
 
@@ -185,3 +194,41 @@ class SessionTests(unittest.TestCase):
         pool.PoolManager.clean = clean = mock.Mock()
         self.obj._cleanup()
         clean.assert_called_once_with(self.obj.pid)
+
+    def test_connect_invokes_pool_manager_get(self):
+        with mock.patch.object(self.obj._pool_manager, 'get') as get:
+            self.obj._connect()
+            get.assert_called_once_with(self.obj.pid, self.obj)
+
+    def test_connect_raises_noidleconnectionserror(self):
+        with mock.patch.object(self.obj._pool_manager, 'get') as get:
+            with mock.patch.object(self.obj._pool_manager, 'is_full') as full:
+                get.side_effect = pool.NoIdleConnectionsError(self.obj.pid)
+                full.return_value = True
+                self.assertRaises(pool.NoIdleConnectionsError,
+                                  self.obj._connect)
+
+    def test_connect_invokes_uri_to_kwargs(self):
+        self.uri_to_kwargs.assert_called_once_with(self.URI)
+
+    def test_connect_returned_the_proper_value(self):
+        self.assertEqual(self.obj.connection, self.conn)
+
+    def test_status_is_ready_by_default(self):
+        self.assertEqual(self.obj._status, self.obj.READY)
+
+    def test_status_when_not_ready(self):
+        self.conn.status = self.obj.SETUP
+        self.assertEqual(self.obj._status, self.obj.SETUP)
+
+    def test_get_named_cursor_sets_scrollable(self):
+        result = self.obj._get_cursor(self.obj._conn, 'test1')
+        self.assertTrue(result.scrollable)
+
+    def test_get_named_cursor_sets_withhold(self):
+        result = self.obj._get_cursor(self.obj._conn, 'test2')
+        self.assertTrue(result.withhhold)
+
+    @unittest.skipUnless(PYPY, 'connection.reset is PYPY only behavior')
+    def test_connection_reset_in_pypy(self):
+        self.conn.reset.assert_called_once_with()
