2b11c707526a9704548162470fd9dcec429b67c7
Gavin M. Roy <gavinr@aweber.com>
2014-09-03 11:41:57 -0400

    Bump the revision
    [ci skip]

--- queries/__init__.py
+++ queries/__init__.py
@@ -8,7 +8,7 @@ The core `queries.Queries` class will automatically register support for UUIDs,
 Unicode and Unicode arrays.
 
 """
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 version = __version__
 
 import logging
