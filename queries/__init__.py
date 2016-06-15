fe232bde4b48a06039f9c870d9f555cefffd3d93
Dave Shawley <daves@aweber.com>
2016-06-14 23:38:44 -0400

    Bump metadata everywhere.
    First release this year?

--- queries/__init__.py
+++ queries/__init__.py
@@ -8,7 +8,7 @@ The core `queries.Queries` class will automatically register support for UUIDs,
 Unicode and Unicode arrays.
 
 """
-__version__ = '1.8.4'
+__version__ = '1.8.10'
 version = __version__
 
 import logging
