3c1492c117e12188e59b09db93561174643bdb9f
Kevin Vance <kevinv@aweber.com>
2018-01-24 10:52:21 -0500

    Clean up connecting state handling
    - Move connecting state into a set of fds
    - Move fd cleanup into its own function

--- queries/tornado_session.py
+++ queries/tornado_session.py
@@ -158,7 +158,7 @@ class TornadoSession(session.Session):
         """
         self._connections = dict()
         self._cleanup_callback = None
-        self._connecting = dict()
+        self._connecting = set()
         self._cursor_factory = cursor_factory
         self._futures = dict()
         self._ioloop = io_loop or ioloop.IOLoop.current()
@@ -292,7 +292,7 @@ class TornadoSession(session.Session):
         # Add the connection for use in _poll_connection
         fd = connection.fileno()
         self._connections[fd] = connection
-        self._connecting[fd] = True
+        self._connecting.add(fd)
 
         def on_connected(cf):
             """Invoked by the IOLoop when the future is complete for the
@@ -305,7 +305,7 @@ class TornadoSession(session.Session):
                 future.set_exception(cf.exception())
 
             else:
-                self._connecting[fd] = False
+                self._connecting.remove(fd)
                 try:
                     # Add the connection to the pool
                     LOGGER.debug('Connection established for %s', self.pid)
@@ -423,7 +423,6 @@ class TornadoSession(session.Session):
             LOGGER.debug('Error closing the cursor: %s', error)
 
         self._pool_manager.free(self.pid, self._connections[fd])
-        self._ioloop.remove_handler(fd)
 
         # If the cleanup callback exists, remove it
         if self._cleanup_callback:
@@ -434,10 +433,19 @@ class TornadoSession(session.Session):
             self._ioloop.time() + self._pool_idle_ttl + 1,
             self._pool_manager.clean, self.pid)
 
+        self._cleanup_fd(fd)
+
+    def _cleanup_fd(self, fd):
+        """Remove any references to an fd in internal state and remove the fd
+        from the ioloop.
+
+        :param int fd: The connection file descriptor
+        """
+        self._ioloop.remove_handler(fd)
         if fd in self._connections:
             del self._connections[fd]
         if fd in self._connecting:
-            del self._connecting[fd]
+            self._connecting.remove(fd)
         if fd in self._futures:
             del self._futures[fd]
 
@@ -471,15 +479,12 @@ class TornadoSession(session.Session):
         except psycopg2.OperationalError as error:
             if fd in self._futures and not self._futures[fd].done():
                     self._futures[fd].set_exception(error)
-            if fd in self._connecting and self._connecting[fd]:
+            if fd in self._connecting:
                 LOGGER.debug('OperationalError %s while connecting fd %s',
                              error, fd)
-                self._ioloop.remove_handler(fd)
-                self._connections[fd].close()
-                del self._connections[fd]
-                del self._connecting[fd]
-                if fd in self._futures:
-                    del self._futures[fd]
+                connection = self._connections[fd]
+                self._cleanup_fd(fd)
+                connection.close()
         except (psycopg2.Error, psycopg2.Warning) as error:
             if fd in self._futures and not self._futures[fd].done():
                 self._futures[fd].set_exception(error)
