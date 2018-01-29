cc365444378f8dea2a9195bf26ab81df53b5b0ca
Gavin M. Roy <gavinr@aweber.com>
2018-01-29 12:07:45 -0500

    Add more functionality to the example

--- examples/simple-tornado.py
+++ examples/simple-tornado.py
@@ -1,30 +1,46 @@
+import datetime
 import logging
+
+from queries import pool
 import queries
 from tornado import gen, ioloop, web
 
 
 class ExampleHandler(web.RequestHandler):
 
-    def initialize(self):
-        self.session = queries.TornadoSession()
+    SQL = 'SELECT * FROM pg_stat_activity'
 
     @gen.coroutine
     def get(self):
         try:
-            result = yield self.session.query('SELECT * FROM names')
+            result = yield self.application.session.query(self.SQL)
         except queries.OperationalError as error:
             logging.error('Error connecting to the database: %s', error)
             raise web.HTTPError(503)
 
-        self.finish({'data': result.items()})
+        rows = []
+        for row in result.items():
+            row = dict([(k, v.isoformat()
+                         if isinstance(v, datetime.datetime) else v)
+                        for k, v in row.items()])
+            rows.append(row)
         result.free()
+        self.finish({'pg_stat_activity': rows})
 
 
-application = web.Application([
-    (r'/', ExampleHandler),
-])
+class ReportHandler(web.RequestHandler):
+
+    @gen.coroutine
+    def get(self):
+        self.finish(pool.PoolManager.report())
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    application.listen(8888)
+    logging.basicConfig(level=logging.DEBUG)
+    application = web.Application([
+        (r'/', ExampleHandler),
+        (r'/report', ReportHandler)
+    ], debug=True)
+    application.session = queries.TornadoSession()
+    application.listen(8000)
     ioloop.IOLoop.instance().start()
