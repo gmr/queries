5a5ff40418f626f7ef271783846195af11f35afd
Gavin M. Roy <gavinr@aweber.com>
2020-03-09 13:34:35 -0400

    Bump the version for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
 
 setuptools.setup(
     name='queries',
-    version='2.0.1',
+    version='2.1.0',
     description='Simplified PostgreSQL client built upon Psycopg2',
     long_description=open('README.rst').read(),
     maintainer='Gavin M. Roy',
