3c68d9767d87466ca56852236655ae66ca0d89f7
Dave Shawley <daves@aweber.com>
2021-11-16 09:58:45 -0500

    Adjust psycopg2 constraint to allow 2.9.x

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import setuptools
 
 # PYPY vs cpython
 if platform.python_implementation() == 'PyPy':
-    install_requires = ['psycopg2cffi>=2.7.2,<2.9']
+    install_requires = ['psycopg2cffi>=2.7.2,<3']
 else:
-    install_requires = ['psycopg2>=2.5.1,<2.9']
+    install_requires = ['psycopg2>=2.5.1,<3']
 
 # Install tornado if generating docs on readthedocs
 if os.environ.get('READTHEDOCS', None) == 'True':
